6cb63233243436d6b912b853a9f96acebc9c2b78
teekayulazy-blip <teekayulazy@gmail.com>
2026-03-19 20:24:18 +0700

    Notify on script load completion
    Add notification for successful script load in Lua.

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
-# my-script
+print("Script Loaded 🔥")
+
+-- ตัวอย่างเพิ่มนิดหน่อย
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Script Hub",
+    Text = "โหลดสคริปสำเร็จ!",
+    Duration = 5
+})
